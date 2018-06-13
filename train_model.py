3fa26a30ff391fe1f55187f373f10150ddf5b7e4
Dominic Jack <thedomjack@gmail.com>
2018-06-13 14:24:40 +1000

    changed default learning rate

--- train_model.py
+++ train_model.py
@@ -7,7 +7,7 @@ from tf_template import TrainModel
 
 def get_train_model(
         batch_size=8, max_steps=150000,
-        optimizer_key='adam', learning_rate=1e-6):
+        optimizer_key='adam', learning_rate=1e-4):
     from tf_template.deserialize import deserialize_optimization_op_fn
     return TrainModel(deserialize_optimization_op_fn(
         key=optimizer_key, learning_rate=learning_rate), batch_size, max_steps)
