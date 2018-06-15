0cb620bd86342bc99300778cf8072c3fe495d876
Dominic Jack <thedomjack@gmail.com>
2018-06-15 10:32:22 +1000

    customized fpi kwargs

--- inference_model/crfrnn_wrapper.py
+++ inference_model/crfrnn_wrapper.py
@@ -9,15 +9,18 @@ from crfrnn.ops import unrolled_crf_rnn
 
 
 class CrfRnnInferenceModel(SegmentationInferenceModel):
-    def __init__(self, base_model, warm_start_settings=None, **crf_rnn_kwargs):
+    def __init__(
+            self, base_model, warm_start_settings=None, explicit_loop=False,
+            num_iterations=10):
         self._base_model = base_model
         self._warm_start_settings = warm_start_settings
-        self._crf_rnn_kwargs = crf_rnn_kwargs
+        self._fpi_kwargs = dict(
+            explicit_loop=explicit_loop, num_iterations=num_iterations)
 
     def get_inference(self, features, mode):
         image = features['image']
         logits = self._base_model.get_inference(features, mode)
-        logits = unrolled_crf_rnn(logits, image, **self._crf_rnn_kwargs)
+        logits = unrolled_crf_rnn(logits, image, fpi_kwargs=self._fpi_kwargs)
         return logits
 
     def get_warm_start_settings(self):
